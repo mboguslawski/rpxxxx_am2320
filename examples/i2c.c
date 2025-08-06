373c3308d62f60407a259bc211c7f2e75cf9e755
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-08-06 16:54:44 +0200

    Add license

--- examples/i2c.c
+++ examples/i2c.c
@@ -1,3 +1,39 @@
+/*
+i2c.c
+Example usage of AM2320 i2c driver.
+
+Copyright (C) 2025 Mateusz Bogusławski, E: mateusz.boguslawski@ibnet.pl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+
+Connections:
+AM2320   RPxxxx
+ GND  <->  GND
+ VCC  <->  +3.3V
+ SCL  <-> GPIO 5 (attach 10k pull-up resistor)
+ SDA  <-> GPIO 4 (attach 10k pull-up resistor)
+
+In CMakeLists.txt:
+- Add rpxxxx_am2320 directory with:
+	add_subdirectory(lib/rpxxxx_am2320/src)
+- Add rpxxxx_am2320 library:
+	add "rpxxxx_am2320" entry inside target_link_libraries()
+- Make sure USB is enabled (to see output in serial monitor)
+	pico_enable_stdio_usb(temp2320 1)
+
+Expected behaviour:
+Every 500ms temperature value in 'C and humidity in % will be send via usb (and printed in serial monitor).
+*/
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
--- src/rpxxxx_am2320.h
+++ src/rpxxxx_am2320.h
@@ -1,3 +1,21 @@
+/*
+rpxxxx_am2320.h
+Header file for AM2320 driver compatible with pico c sdk.
+
+Copyright (C) 2025 Mateusz Bogusławski, E: mateusz.boguslawski@ibnet.pl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+
 #ifndef RPXXXX_AM2320_H
 #define RPXXXX_AM2320_H
 
